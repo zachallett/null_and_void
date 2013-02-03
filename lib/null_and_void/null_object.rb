7f3c2deb1374175635e1f0e8e412831f870d18e8
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-03 14:52:25 -0600

    Allow NullObject to return itself for all unknown method calls

--- lib/null_and_void/null_object.rb
+++ lib/null_and_void/null_object.rb
@@ -3,5 +3,9 @@ require 'singleton'
 module NullAndVoid
   class NullObject
     include Singleton
+
+    def method_missing(*args, &block)
+      self
+    end
   end
 end
--- spec/null_object_spec.rb
+++ spec/null_object_spec.rb
@@ -7,4 +7,8 @@ describe NullAndVoid::NullObject do
   it 'is a singleton' do
     null_object == NullAndVoid::NullObject.instance
   end
+
+  it 'returns itself for all unknown method calls' do
+    null_object.foobar.should eql null_object
+  end
 end
