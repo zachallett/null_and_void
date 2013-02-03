4b73cec3b6f0e0d607c85de9bf3c6a9c64ebc018
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-03 15:42:18 -0600

    Add Convertible.to_i which returns 0

--- lib/null_and_void/convertible.rb
+++ lib/null_and_void/convertible.rb
@@ -1,4 +1,7 @@
 module NullAndVoid
   module Convertible
+    def to_i
+      0
+    end
   end
 end
--- spec/convertible_spec.rb
+++ spec/convertible_spec.rb
@@ -7,4 +7,9 @@ end
 
 describe NullAndVoid::Convertible do
   let(:null_object) { NullObject.new }
+
+  it 'is zero when the attempt is made to convert it to an Integer' do
+    null_object.to_i.should be_an Integer
+    null_object.to_i.should be_zero
+  end
 end
