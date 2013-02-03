b6115de35a628d14851a3866667304be1d21a00f
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-03 14:52:05 -0600

    Whitespace

--- spec/null_object_spec.rb
+++ spec/null_object_spec.rb
@@ -2,7 +2,9 @@ require 'rspectacular'
 require 'null_and_void/null_object'
 
 describe NullAndVoid::NullObject do
+  let(:null_object) { NullAndVoid::NullObject.instance }
+
   it 'is a singleton' do
-    NullAndVoid::NullObject.instance == NullAndVoid::NullObject.instance
+    null_object == NullAndVoid::NullObject.instance
   end
 end
