6b248fd3f12e9d486dcd90ce5bf68826369d661a
Jeff Felchner <accounts+git@thekompanee.com>
2013-02-03 21:15:39 -0600

    Fix paths to modules in NullAndVoid::Nullified

--- lib/null_and_void/nullified.rb
+++ lib/null_and_void/nullified.rb
@@ -1,16 +1,17 @@
 require 'singleton'
 require 'null_and_void/stubbable'
 require 'null_and_void/convertible'
+require 'null_and_void/falsifiable'
 require 'null_and_void/introspectable'
 
 module NullAndVoid
   module Nullified
     def self.included(base)
-      base.send(:include, Singleton)
-      base.send(:include, Stubbable)
-      base.send(:include, Convertible)
-      base.send(:include, Falsifiable)
-      base.send(:include, Introspectable)
+      base.send(:include, ::NullAndVoid::Singleton)
+      base.send(:include, ::NullAndVoid::Stubbable)
+      base.send(:include, ::NullAndVoid::Convertible)
+      base.send(:include, ::NullAndVoid::Falsifiable)
+      base.send(:include, ::NullAndVoid::Introspectable)
     end
   end
 end
