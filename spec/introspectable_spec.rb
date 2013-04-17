eb291ef89a6c96b0bc1de343ee61f9be13cff356
Jeff Felchner <accounts+git@thekompanee.com>
2013-04-17 12:05:54 -0500

    Fix spelling mistake

--- spec/introspectable_spec.rb
+++ spec/introspectable_spec.rb
@@ -1,12 +1,12 @@
 require 'rspectacular'
 require 'null_and_void/introspectable'
 
-class MyNullOjbect
+class MyNullObject
   include NullAndVoid::Introspectable
 end
 
 describe NullAndVoid::Introspectable do
-  let(:null_object) { MyNullOjbect.new }
+  let(:null_object) { MyNullObject.new }
 
   it 'knows it is a null object' do
     null_object.should be_null_object
