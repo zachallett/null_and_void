1d51b038e15cc6d5c433c32720986fea6d799150
Jeff Felchner <accounts+git@thekompanee.com>
2013-04-17 12:06:24 -0500

    Allow null objects to inspect themselves

--- lib/null_and_void/introspectable.rb
+++ lib/null_and_void/introspectable.rb
@@ -3,5 +3,9 @@ module NullAndVoid
     def null_object?
       true
     end
+
+    def inspect
+      "#<#{self.class.name}:0x#{(__id__ * 2).to_s(16)}>"
+    end
   end
 end
--- spec/introspectable_spec.rb
+++ spec/introspectable_spec.rb
@@ -11,4 +11,8 @@ describe NullAndVoid::Introspectable do
   it 'knows it is a null object' do
     null_object.should be_null_object
   end
+
+  it 'can inspect itself' do
+    null_object.inspect.should match /\A#<MyNullObject:0x[a-f0-9]{12}>\z/
+  end
 end
