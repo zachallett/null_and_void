98974facd2c6f9211bddb8b01c403758ae101b4e
Jeff Felchner <accounts+git@thekompanee.com>
2015-05-12 21:49:09 -0500

    Added: NullAndVoid::Persistable

---
+++ lib/null_and_void/persistable.rb
@@ -0,0 +1,35 @@
+module NullAndVoid
+  module Persistable
+    def readonly?
+      true
+    end
+
+    def persisted?
+      false
+    end
+
+    def new_record?
+      true
+    end
+
+    def destroyed?
+      true
+    end
+
+    def all
+      []
+    end
+
+    def to_key
+      []
+    end
+
+    def to_param
+      ''
+    end
+
+    def valid?
+      true
+    end
+  end
+end
